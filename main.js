8a6edcc09fb66041119f50971cfb45386311ea21
donkeysharp <serguimant@gmail.com>
2015-07-10 20:17:25 -0400

    added phaser libraries

--- main.js
+++ main.js
@@ -1,41 +1,110 @@
-var mainState = ( function () {
-    
-    var preload = function () {
-	// load sprite images:
-	// game.load.spritesheet('name', 'directory', sizex, sizey);	
+var game = new Phaser.Game(800, 400, Phaser.AUTO, 'test', null, true, false);
+
+var BasicGame = function (game) { };
+
+BasicGame.Boot = function (game) { };
+
+var isoGroup, player;
+
+BasicGame.Boot.prototype ={
+  preload: function () {
+    game.load.image('cube_', '../assets/cube.png');
+    game.load.image('ground', '../assets/ground_tile.png');
+
+    game.time.advancedTiming = true;
+    // Add and enable the plug-in.
+    game.plugins.add(new Phaser.Plugin.Isometric(game));
+
+    // Start the IsoArcade physics system.
+    game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
+
+    // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
+    // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
+    game.iso.anchor.setTo(0.5, 0.2);
+  },
+  create: function () {
+    // Create a group for our tiles, so we can use Group.sort
+    isoGroup = game.add.group();
+
+    // Set the global gravity for IsoArcade.
+    game.physics.isoArcade.gravity.setTo(0, 0, -500);
+
+    // Let's make a load of cubes on a grid, but do it back-to-front so they get added out of order.
+    var cube;
+    for (var xx = 256; xx > 0; xx -= 80) {
+      for (var yy = 256; yy > 0; yy -= 80) {
+        // Create a cube using the new game.add.isoSprite factory method at the specified position.
+        // The last parameter is the group you want to add it to (just like game.add.sprite)
+        cube = game.add.isoSprite(xx, yy, -80, 'ground', 0, isoGroup);
+        // cube.anchor.set(0.5);
+        // Enable the physics body on this cube.
+        // game.physics.isoArcade.enable(cube);
+        // Collide with the world bounds so it doesn't go falling forever or fly off the screen!
+        // cube.body.collideWorldBounds = true;
+        // Add a full bounce on the x and y axes, and a bit on the z axis.
+        // cube.body.bounce.set(1, 1, 0.2);
+        // Add some X and Y drag to make cubes slow down after being pushed.
+        // cube.body.drag.set(100, 100, 0);
+      }
     }
-    
-    var create = function () {
-	// start the physics engine
-        // game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	// tiled image as background
-        // game.add.tileSprite(0,0, width, height, image, 0);
+    // Create another cube as our 'player', and set it up just like the cubes above.
+    player = game.add.isoSprite(128, 128, 0, 'cube_', 0, isoGroup);
+    player.tint = 0x00ff00;
+    player.anchor.set(0.5);
+    game.physics.isoArcade.enable(player);
+    player.body.collideWorldBounds = true;
 
-        game.stage.backgroundColor = "#5f5";
+    // Set up our controls.
+    this.cursors = game.input.keyboard.createCursorKeys();
 
-	// create a sprite:
-	// var pepe = game.add.sprite(x, y, 'pepe');
-	// pepe.anchor.setTo(0.5,0.5);
+    this.game.input.keyboard.addKeyCapture([
+      Phaser.Keyboard.LEFT,
+      Phaser.Keyboard.RIGHT,
+      Phaser.Keyboard.UP,
+      Phaser.Keyboard.DOWN,
+      Phaser.Keyboard.SPACEBAR
+    ]);
 
-	// enable physics for pepe
-	// game.physics.enable(pepe, Phaser.Physics.ARCADE);
+    var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-	// add animation to pepe.
-	// sprite.animations.add('walk', [0,1,2,1], fps);
+    space.onDown.add(function () {
+      player.body.velocity.z = 300;
+    }, this);
+  },
+  update: function () {
+    // Move the player at this speed.
+    var speed = 100;
+
+    if (this.cursors.up.isDown) {
+      player.body.velocity.y = -speed;
     }
-    
-    var update = function () {
-    };
-    
-    return { preload : preload,
-             create : create,
-             update : update };
-    
-})();
+    else if (this.cursors.down.isDown) {
+      player.body.velocity.y = speed;
+    }
+    else {
+      player.body.velocity.y = 0;
+    }
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
-game.state.add('main', mainState);
-game.state.start('main');
-// width = 800. accessible from game.world.width
-// height = 600 acccessible from game.world.height
+    if (this.cursors.left.isDown) {
+      player.body.velocity.x = -speed;
+    }
+    else if (this.cursors.right.isDown) {
+      player.body.velocity.x = speed;
+    }
+    else {
+      player.body.velocity.x = 0;
+    }
+
+    // Our collision and sorting code again.
+    // game.physics.isoArcade.collide(isoGroup);
+    game.iso.topologicalSort(isoGroup);
+  },
+  render: function () {
+    game.debug.text("Move with cursors, jump with space!", 2, 36, "#ffffff");
+    game.debug.text(game.time.fps || '--', 2, 14, "#a7aebe");
+  }
+};
+
+game.state.add('Boot', BasicGame.Boot);
+game.state.start('Boot');
