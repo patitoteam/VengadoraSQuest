0cb35e3df8d381b6bf740c618e55eb0b9ba42341
donkeysharp <serguimant@gmail.com>
2015-07-12 11:23:07 -0400

    Improved initial page

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function() {
-  window.game = new Phaser.Game(700, 500, Phaser.AUTO, 'test', null, false, true);
+  window.game = new Phaser.Game(700, 500, Phaser.AUTO, 'game', null, false, true);
 
   game.state.add('Level1', BasicGame.Level1);
   game.state.add('Level2', BasicGame.Level2);
